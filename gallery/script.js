ba0b4b39a617535ed4e5f65b90410b4b000d1ec3
tushdemort <40672760+tushdemort@users.noreply.github.com>
2023-04-07 22:21:33 +0000

    im final

--- gallery/script.js
+++ gallery/script.js
@@ -11,21 +11,23 @@
     const assets = [
 
     "../images/gallery_full/1st_column/1.jpeg?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877042868118769695/6606610f-6798-46db-8be9-c45e43413471.png?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877043183861780520/d0d1c6c1-ad5a-409d-bba2-90b87774d12a.png?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877043285233913926/f00e4986-2421-4af5-9ce1-37c1964bec38.png?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877043381866475580/6facf6ef-2f27-41ab-b8cc-1e32dfbfcf79.png?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877043786872651786/754e0049-9296-4601-965e-f598ec4f5e0d.png?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877044040707764274/71156726-54f2-4151-a260-805a1c5f26bf.png?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877044137738772560/224988fa-dfad-497d-8a9d-a012f85d8944.png?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877044303619301396/c2a31331-e916-4871-836b-414957a5dbb9.png?width=882&height=588",
-    "https://media.discordapp.net/attachments/877042586475446272/877046659027185674/73a28f61-40ff-4f63-b4c3-5705e61d1e67.png?width=609&height=406",
-    "https://media.discordapp.net/attachments/877042586475446272/877046162585174037/e0c83ee6-fbbb-445a-979f-d915bcc4c2ba.png",
-    "https://media.discordapp.net/attachments/877042586475446272/877045224390664223/3296500d-5527-41fe-87f5-ae7282962c7d.png?width=609&height=406",
-    "https://media.discordapp.net/attachments/877042586475446272/877045007452889089/693c3bb4-778e-4187-b503-ef6aef5b64d5.png?width=609&height=406",
-    "https://media.discordapp.net/attachments/877042586475446272/877044689025527838/6ef59e3d-7006-4e9b-a532-f58bd9c22c3c.png?width=609&height=406",
-    "https://media.discordapp.net/attachments/877042586475446272/877044451124576316/11e01140-a467-49c0-bcd4-43292cfd069a.png?width=609&height=406",
-    "https://media.discordapp.net/attachments/877042586475446272/877044392131698718/27cf59a0-1b7c-458d-b4d3-76f69b21c09f.png?width=609&height=406",
+    "../images/gallery_full/1st_column/2.jpeg?width=882&height=588",
+    "../images/gallery_full/1st_column/3.jpeg?width=882&height=588",
+    "../images/gallery_full/1st_column/4.jpeg?width=882&height=588",
+    "../images/gallery_full/2st_column/1.jpeg?width=882&height=588",
+    "../images/gallery_full/2st_column/2.jpeg?width=882&height=588",
+    "../images/gallery_full/2st_column/3.jpeg?width=882&height=588",
+    "../images/gallery_full/2st_column/4.jpeg?width=882&height=588",
+    "../images/gallery_full/3st_column/1.jpeg?width=882&height=588",
+    "../images/gallery_full/3st_column/2.jpeg?width=882&height=588",
+    "../images/gallery_full/3st_column/3.jpeg?width=882&height=588",
+    "../images/gallery_full/3st_column/4.jpeg?width=882&height=588",
+    "../images/gallery_full/4st_column/1.jpeg?width=882&height=588",
+    "../images/gallery_full/4st_column/2.jpeg?width=882&height=588",
+    "../images/gallery_full/4st_column/3.jpeg?width=882&height=588",
+    "../images/gallery_full/4st_column/4.jpeg?width=882&height=588",
+
+
   ];
 
 
